8e4868195dc07e67ccd956e4ac73aa8452c325d5
TaHuC <TaHuC@hotmail.com>
2017-02-18 19:05:12 +0200

    Complate this task

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CountSubstringOccurrences
+{
+	public class MainClass
+	{
+		public static void Main()
+		{
+			var getText = Console.ReadLine().ToLower();
+			var getString = Console.ReadLine().ToLower();
+
+			var count = 0;
+
+			int i = getText.IndexOf(getString);
+			int old = i;
+
+			if (i != -1)
+			{
+				count++;
+			}
+
+			while (true)
+			{
+				i = getText.IndexOf(getString, i+1);
+
+				if (i > old)
+				{
+					count++;
+					old = i;
+				}
+				else
+				{
+					break;
+				}
+			}
+
+			Console.WriteLine(count);
+		}
+	}
+}
